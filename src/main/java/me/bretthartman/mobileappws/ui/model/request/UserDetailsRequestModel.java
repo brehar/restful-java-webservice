e08fa5a70026c5fae0e47e3e10ed67911db498fe
Brett Hartman <brett.hartman@mac.com>
2019-03-16 08:50:15 -0700

    Revise request/response classes for POST User to integrate Lombok

--- src/main/java/me/bretthartman/mobileappws/ui/model/request/UserDetailsRequestModel.java
+++ src/main/java/me/bretthartman/mobileappws/ui/model/request/UserDetailsRequestModel.java
@@ -1,5 +1,10 @@
 package me.bretthartman.mobileappws.ui.model.request;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserDetailsRequestModel {
   private String firstName;
   private String lastName;
