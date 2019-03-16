f590af3bac8eea75b7b12cecfe71ba8ee12c4d79
Brett Hartman <brett.hartman@mac.com>
2019-03-16 08:49:01 -0700

    Create request/response classes for POST User and integrate Lombok

--- src/main/java/me/bretthartman/mobileappws/ui/controller/UserController.java
+++ src/main/java/me/bretthartman/mobileappws/ui/controller/UserController.java
@@ -1,9 +1,11 @@
 package me.bretthartman.mobileappws.ui.controller;
 
+import me.bretthartman.mobileappws.ui.model.request.UserDetailsRequestModel;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +18,7 @@ public class UserController {
   }
 
   @PostMapping
-  public String createUser() {
+  public String createUser(@RequestBody UserDetailsRequestModel userDetails) {
     return "create user was called";
   }
 
---
+++ src/main/java/me/bretthartman/mobileappws/ui/model/request/UserDetailsRequestModel.java
@@ -0,0 +1,8 @@
+package me.bretthartman.mobileappws.ui.model.request;
+
+public class UserDetailsRequestModel {
+  private String firstName;
+  private String lastName;
+  private String email;
+  private String password;
+}
